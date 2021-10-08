723ae19b90fd7f6c36789fbad8187f97ea667bb9
egonlr <egonlr@hotmail.com>
2021-10-08 17:40:59 -0300

    Ajustando o visual do Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+        theme: ThemeData(
+            primaryColor: Colors.green[900],
+            accentColor: Colors.blueAccent[700],
+            buttonTheme: ButtonThemeData(
+              buttonColor: Colors.blueAccent[700],
+              textTheme: ButtonTextTheme.primary,
+            ),
+        ),
+        home: Dashboard(),
+    );
+  }
+}
+
+class  Dashboard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Image.asset('images/bytebank_logo.png'),
-          Container(
-              height: 120,
-              width: 100,
-              color: Colors.green,
-              child: Column(
-                children: [
-                  Icon(Icons.people),
-                  Text('Contacts'),
-                ],
-              ))
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: EdgeInsets.all(8.0),
+                height: 100,
+                width: 150,
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(
+                      Icons.people,
+                      color: Colors.white,
+                      size: 24,
+                    ),
+                    Text('Contacts',
+                        style:
+                        TextStyle(color: Colors.white, fontSize: 16.0)),
+                  ],
+                )),
+          )
         ],
       ),
-    ));
+    );
   }
 }
+
