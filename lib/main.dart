bf369ed9de6385b5c1f59255bb58cf4877a179ba
egonlr <egonlr@hotmail.com>
2021-10-14 15:09:52 -0300

    Implementando a tela do formulário

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/dashboard.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
               textTheme: ButtonTextTheme.primary,
             ),
         ),
-        home: Dashboard(),
+        home: ContactForm(),
     );
   }
 }
--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -32,3 +32,4 @@ class ContactsList extends StatelessWidget {
     );
   }
 }
+
