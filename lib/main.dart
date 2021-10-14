990133b0917e2c0cede02291b363124817f11dc1
egonlr <egonlr@hotmail.com>
2021-10-14 14:30:07 -0300

    Implementando a lista de contatos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/dashboard.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -16,50 +18,6 @@ class MyApp extends StatelessWidget {
             ),
         ),
         home: Dashboard(),
-    );
-  }
-}
-
-class  Dashboard extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 100,
-                width: 150,
-                color: Theme.of(context).primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 24,
-                    ),
-                    Text('Contacts',
-                        style:
-                        TextStyle(color: Colors.white, fontSize: 16.0)),
-                  ],
-                )),
-          )
-        ],
-      ),
     );
   }
 }
